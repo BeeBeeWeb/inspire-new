17632ade7cb4b3e38e06cf65bd2d7ed699a7fa69
Chinmay P <chinmay412@gmail.com>
2019-06-24 00:18:57 +0530

    Updates for scrollspy UI, Footer map linking. Commented particle js code in common js file

--- js/common.js
+++ js/common.js
@@ -8,77 +8,77 @@ if (browserVersion <= 4) {
 
 $(document).ready(function () {
 
-  if (['/index.html', '/'].includes(document.location.pathname)) {
+  // if (['/index.html', '/'].includes(document.location.pathname)) {
 
-    particlesJS("particles-js", {
-      particles: {
-        number: { value: 90, density: { enable: false, value_area: 400 } },
-        color: { value: "#5f2400" },
-        shape: {
-          type: "circle",
-          stroke: { width: 0, color: "#000000" },
-          polygon: { nb_sides: 5 },
-          image: { src: "img/github.svg", width: 100, height: 100 }
-        },
-        opacity: {
-          value: 1,
-          random: true,
-          anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
-        },
-        size: {
-          value: 6,
-          random: true,
-          anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
-        },
-        line_linked: {
-          enable: true,
-          distance: 200,
-          color: "#5f2400",
-          opacity: 0.8,
-          width: 1
-        },
-        move: {
-          enable: true,
-          speed: 2,
-          direction: "top-left",
-          random: false,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
-          attract: { enable: false, rotateX: 600, rotateY: 1200 }
-        }
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: { enable: true, mode: "grab" },
-          onclick: { enable: false, mode: "push" },
-          resize: true
-        },
-        modes: {
-          grab: { distance: 250, line_linked: { opacity: 1 } },
-          bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-          repulse: { distance: 200, duration: 0.4 },
-          push: { particles_nb: 4 },
-          remove: { particles_nb: 2 }
-        }
-      },
-      retina_detect: true
-    });
+  //   particlesJS("particles-js", {
+  //     particles: {
+  //       number: { value: 90, density: { enable: false, value_area: 400 } },
+  //       color: { value: "#5f2400" },
+  //       shape: {
+  //         type: "circle",
+  //         stroke: { width: 0, color: "#000000" },
+  //         polygon: { nb_sides: 5 },
+  //         image: { src: "img/github.svg", width: 100, height: 100 }
+  //       },
+  //       opacity: {
+  //         value: 1,
+  //         random: true,
+  //         anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+  //       },
+  //       size: {
+  //         value: 6,
+  //         random: true,
+  //         anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+  //       },
+  //       line_linked: {
+  //         enable: true,
+  //         distance: 200,
+  //         color: "#5f2400",
+  //         opacity: 0.8,
+  //         width: 1
+  //       },
+  //       move: {
+  //         enable: true,
+  //         speed: 2,
+  //         direction: "top-left",
+  //         random: false,
+  //         straight: false,
+  //         out_mode: "out",
+  //         bounce: false,
+  //         attract: { enable: false, rotateX: 600, rotateY: 1200 }
+  //       }
+  //     },
+  //     interactivity: {
+  //       detect_on: "canvas",
+  //       events: {
+  //         onhover: { enable: true, mode: "grab" },
+  //         onclick: { enable: false, mode: "push" },
+  //         resize: true
+  //       },
+  //       modes: {
+  //         grab: { distance: 250, line_linked: { opacity: 1 } },
+  //         bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+  //         repulse: { distance: 200, duration: 0.4 },
+  //         push: { particles_nb: 4 },
+  //         remove: { particles_nb: 2 }
+  //       }
+  //     },
+  //     retina_detect: true
+  //   });
     
-  }
+  // }
 
   $('a[href*="#"]').on('click', function (e) {
-    e.preventDefault()
+    e.preventDefault();
 
     $('html, body').animate(
       {
-        scrollTop: $($(this).attr('href')).offset().top,
+        scrollTop: $($(this).attr('href')).offset().top - 20,
       },
       500,
       'linear'
-    )
-  })
+    );
+  });
 
   $(".design-link").click(function(){
     $(".collapse").collapse('toggle');
