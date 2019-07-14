49d6c990521752154ef6ecf6fc0c92bb7dae7c11
Chinmay P <chinmay412@gmail.com>
2019-07-14 11:38:14 +0530

    Saturday Meeting changes for Contact Us and Careers page. Also added phpmailer plugin folder and working on forms.

---
+++ send.php
@@ -0,0 +1,37 @@
+<?php
+require '/phpMailer/PHPMailerAutoload.php';
+header('Content-Type: application/json');
+$fname = $_POST['firstName'];
+$lname = $_POST['lastName'];
+$tel = $_POST['phoneNumber'];
+$cname = $_POST['companyName'];
+$mail = $_POST['emailId'];
+$role = $_POST['yourRole'];
+$message = $_POST['enquiryDetails'];
+
+$mail = new PHPMailer;
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'inspiregrouppune@gmail.com';                 // SMTP username
+$mail->Password = 'inspire1234';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom($_POST['emailId'], $_POST['firstName']);
+// Add a recipient
+$mail->addAddress('chinmay412@gmail.com');               // Name is optional
+
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Testing PHPmailer email sending functionality.';
+$mail->Body    = "$message Phone $tel";
+$mail->AltBody = $message;
+if(!$mail->send()) {
+echo 'Message could not be sent.';
+         echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+    echo 'Message has been sent';
+    }
+    ?>
